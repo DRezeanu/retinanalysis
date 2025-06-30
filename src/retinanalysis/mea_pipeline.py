264b87be6fb247b18d988b306a8367fe9ebed792
DRezeanu <DRezeanu@uw.edu>
2025-06-30 16:18:03 -0700

    major updates to mea_pipeline
    MEA Pipeline now automatically adds type information to the protocol. cluster match (in vision_utils) can now use isi and rgb STA traces to improve classification transfer between noise chunks.

--- src/retinanalysis/mea_pipeline.py
+++ src/retinanalysis/mea_pipeline.py
@@ -4,6 +4,8 @@ from retinanalysis.stim import StimBlock
 from retinanalysis.analysis_chunk import AnalysisChunk
 import visionloader as vl
 import retinanalysis.vision_utils as vu
+import os
+from retinanalysis.settings import NAS_ANALYSIS_DIR
 
 
 class MEAPipeline:
@@ -13,55 +15,117 @@ class MEAPipeline:
         self.response_block = response_block
         self.analysis_chunk = analysis_chunk
 
-        self.match_dict = self.cluster_match()
+        self.match_dict = vu.cluster_match(self.analysis_chunk.vcd, self.response_block.vcd,
+                                           use_isi = False)
+        
+        self.add_types_to_protocol()
+    
+    def classification_transfer(self, target_chunk: str, ss_version: str = None, input_typing_file: str = None, 
+                                output_typing_file: str = 'RA_autoClassification.txt', **kwargs):
+        
+        """Transfer classification between analysis chunk and another noise chunk
+        Inputs:
+            target_chunk: str such as 'chunk2'
+            ss_version: str such as 'kilosort2.5', if None, uses same ss_version as analysis_chunk
+            input_typing_file: str, filename of classification file to use, if None will use
+                               the first typing file in analysis_chunk.typing_files
+            output_typing_file: str, filename of classification file to export, default is
+                                RA_autoClassification.txt
 
-    def cluster_match(self, corr_cutoff: float = 0.8, method: str = 'full'):
-        corr_arr: np.ndarray = vu.ei_corr(self.analysis_chunk.vcd, self.response_block.vcd, method = method)
-        # Methods: full, space, power. Space uses no time dimension, power squares the response
+        Kwargs to pass to cluster_match:
+            use_isi: bool, default = false
+            corr_cutoff: float, default = 0.8
+            method: str, default = 'full'"""
+        
+        # Flag if there are no typing files available for this analysis chunk
+        if len(self.analysis_chunk.typing_files) == 0:
+            raise FileNotFoundError("No typing files available for this analysis chunk")
+        
+        if target_chunk == self.analysis_chunk.chunk_name:
+            raise Exception(f"Target chunk ({target_chunk}) cannot be the same as analysis chunk")
 
-        match_dict = dict()
-        match_count = 0
-        bad_match_count = 0
+        # If no input typing file is specified, use typing_file_0
+        if input_typing_file is None:
+            input_typing_file = self.analysis_chunk.typing_files[0] 
 
-        analysis_ids = self.analysis_chunk.cell_ids
-        response_ids = self.response_block.cell_ids
+        # Flag if input typing file is not actually part of the current analysis chunk
+        if input_typing_file not in self.analysis_chunk.typing_files:
+            raise FileNotFoundError("Input typing file not found in current chunk")         
 
-        for idx, ref_cell in enumerate(analysis_ids):
-            sorted_corr = np.sort(corr_arr[idx,:])
-            sorted_corr = np.flip(sorted_corr)
+        # If no spike sorting version is given, use same ss_version as analysis chunk
+        if ss_version is None:
+            ss_version = self.analysis_chunk.ss_version
+        
+        print(f"Cluster matching {self.analysis_chunk.chunk_name} with {target_chunk}\n")
+        
+        # Cluster Match
+        target_vcd = vu.get_analysis_vcd(self.analysis_chunk.exp_name, target_chunk, ss_version)
+        target_ids = target_vcd.get_cell_ids()
 
-            max_corr = sorted_corr[0]
-            next_max_corr = sorted_corr[1]
-            max_ind = np.argmax(corr_arr[idx,:])
-            max_rev_ind = np.argmax(corr_arr[:,max_ind])
+        match_dict = vu.cluster_match(self.analysis_chunk.vcd, target_vcd, **kwargs)
+        
+        # Create classification file and drop it in the destination path
+        input_file_path = os.path.join(NAS_ANALYSIS_DIR, self.analysis_chunk.exp_name,
+                                       self.analysis_chunk.chunk_name, self.analysis_chunk.ss_version,
+                                       input_typing_file)
+        
+        destination_file_path = os.path.join(NAS_ANALYSIS_DIR, self.analysis_chunk.exp_name,
+                                             target_chunk, ss_version, output_typing_file)
 
-            if max_corr > corr_cutoff:
-                if next_max_corr > (max_corr*0.90):
-                    bad_match_count += 1
-                elif analysis_ids[max_rev_ind] != ref_cell:
-                    bad_match_count += 1
+        
+        matched_count = 0
+        unmatched_count = 0
+        input_classification_dict = vu.create_dictionary_from_file(input_file_path, delimiter = ' ')
+
+        with open(destination_file_path, mode='w') as output_file:
+            for key in match_dict.keys():
+                matched_count += 1
+                print(match_dict[key], input_classification_dict[key], file = output_file)
+
+        partial_output = vu.create_dictionary_from_file(destination_file_path, delimiter = ' ')
+
+        with open(destination_file_path, mode = 'a') as output_file:
+            for id in target_ids:
+                if id in partial_output:
+                    pass
                 else:
-                    match_dict[ref_cell] = response_ids[max_ind]
-                    match_count += 1    
+                    print(id, 'All/unmatched', file = output_file)
+                    unmatched_count += 1
 
+        print(f"\nTarget clusters matched: {matched_count}\nTarget clusters unmatched: {unmatched_count}\n")
+        print(f"Classification file {output_typing_file} created at: {destination_file_path}")
+
+        return match_dict
+
+    def add_types_to_protocol(self, typing_file: str = None):
+
+        if typing_file is None:
+            typing_file = 0
+        else:
+            try:
+                typing_file = self.analysis_chunk.typing_files.index(typing_file)
+            except:
+                raise FileNotFoundError(f"{typing_file} Not Found in Analysis Chunk")
+        
+        type_dict = dict()
+        for id in self.analysis_chunk.cell_params_df['cell_id']:
+            if id in self.match_dict:
+                type_dict[self.match_dict[id]] = self.analysis_chunk.cell_params_df.query('cell_id == @id')[f'typing_file_{typing_file}'].values[0]
+        
+        for id in self.response_block.df_spike_times.index:
+            if id in type_dict:
+                pass
             else:
-                bad_match_count += 1
-        
-        percent_good = match_count/len(analysis_ids)
-        percent_bad = bad_match_count/len(analysis_ids)
+                type_dict[id] = "Unmatched"
 
-        print(f"\nRef clusters matched: {match_count}")
-        print(f"Ref clusters unmatched: {bad_match_count}")
-        print(f"{np.round(percent_good*100, 2)}% matched, {np.round(percent_bad*100, 2)}% unmatched.")
-        
-        self.match_dict = dict(sorted(match_dict.items()))
+        type_dict_sorted = dict(sorted(type_dict.items()))
 
-        return self.match_dict
+        self.response_block.df_spike_times['cell_type'] = type_dict_sorted
 
-
-    def get_type_dict(self):
-        pass
-
-    def purpura_spike_distance(self, spike_train_1, spike_train_2):
-        pass
+    def __repr__(self):
+        str_self = f"{self.__class__.__name__} with properties:\n"
+        str_self += f"  stim_block and response_block from: {os.path.splitext(self.stim_block.protocol_name)[1][1:]}\n"
+        str_self += f"  analysis_chunk: {self.analysis_chunk.chunk_name}\n"
+        str_self += f"  match_dict: with {self.analysis_chunk.chunk_name}_id : {os.path.splitext(self.stim_block.protocol_name)[1][1:]}_id"
+        return str_self
 
--- src/retinanalysis/vision_utils.py
+++ src/retinanalysis/vision_utils.py
@@ -4,227 +4,116 @@ import numpy as np
 import retinanalysis.datajoint_utils as dju
 import visionloader as vl
 
-NAS_DATA_DIR = mea_config['data']
+
+NAS_DATA_DIR = mea_config['data'] 
 NAS_ANALYSIS_DIR = mea_config['analysis']
 META_DIR = mea_config['meta']
 TAGS_DIR = mea_config['tags']
 H5_DIR = mea_config['h5']
 USER = mea_config['user']
 
+def get_analysis_vcd(exp_name, chunk_name, ss_version):
+
+        data_path = os.path.join(NAS_ANALYSIS_DIR, exp_name, chunk_name, ss_version)
+        vcd = vl.load_vision_data(data_path, ss_version, include_ei = True,
+                                  include_noise = False, include_sta = False,
+                                  include_params = True, include_runtimemovie_params = True,
+                                  include_neurons = True)
+        
+        return vcd
+
+def get_protocol_vcd(exp_name, datafile_name, ss_version):
+        data_path = os.path.join(NAS_DATA_DIR, exp_name, datafile_name, ss_version)
+
+        vcd = vl.load_vision_data(
+            data_path, datafile_name, 
+            include_ei = True, include_neurons = True
+            )
+        
+        return vcd
+
+
+def cluster_match(ref_vcd: vl.VisionCellDataTable, test_vcd: vl.VisionCellDataTable,
+                corr_cutoff: float = 0.8, method: str = 'full', use_isi: bool = False,
+                use_rgb: bool = False):
+        
+        corr_arr: np.ndarray = ei_corr(ref_vcd, test_vcd, method = method)
+        # Methods: full, space, power. Space uses no time dimension, power squares the response
+
+        match_dict = dict()
+        match_count = 0
+        bad_match_count = 0
+        isi_corr = 1
+        rgb_corr = 1
+
+        ref_ids = ref_vcd.get_cell_ids()
+        test_ids = test_vcd.get_cell_ids()
+
+        for idx, ref_cell in enumerate(ref_ids):
+            sorted_corr = np.sort(corr_arr[idx,:])
+            sorted_corr = np.flip(sorted_corr)
+
+            max_corr = sorted_corr[0]
+            next_max_corr = sorted_corr[1]
+            max_ind = np.argmax(corr_arr[idx,:])
+            max_rev_ind = np.argmax(corr_arr[:,max_ind])
+
+            if max_corr > corr_cutoff:
+                if use_rgb:
+                    ref_rg = ref_vcd.main_datatable[ref_cell]['GreenTimeCourse']
+                    ref_b = ref_vcd.main_datatable[ref_cell]['BlueTimeCourse']
+                    test_rg = test_vcd.main_datatable[test_ids[max_ind]]['GreenTimeCourse']
+                    test_b = test_vcd.main_datatable[test_ids[max_ind]]['BlueTimeCourse']
+                    
+                    ref_rgb = np.concatenate([ref_rg, ref_b])
+                    test_rgb = np.concatenate([test_rg, test_b])
+                    np.nan_to_num(ref_rgb, copy=False, nan=0, neginf=0, posinf=0)
+                    np.nan_to_num(test_rgb, copy = False, nan=0, neginf=0, posinf=0)
+                    
+                    rgb_corr = np.corrcoef(ref_rgb, test_rgb)[0,1]
+
+                if use_isi:
+                    ref_isi = ref_vcd.get_acf_numpairs_for_cell(ref_cell)
+                    match_isi = test_vcd.get_acf_numpairs_for_cell(test_ids[max_ind])
+                    np.nan_to_num(ref_isi, copy=False, nan=0, neginf=0, posinf=0)
+                    np.nan_to_num(match_isi, copy = False, nan=0, neginf=0, posinf=0)
+
+                    isi_corr = np.corrcoef(ref_isi, match_isi)[0,1]
+
+                if next_max_corr > (max_corr*0.90) or isi_corr < 0.3 or rgb_corr < 0.3:
+                    bad_match_count += 1
+                elif ref_ids[max_rev_ind] != ref_cell:
+                    bad_match_count += 1
+                else:
+                    match_dict[ref_cell] = test_ids[max_ind]
+                    match_count += 1    
+
+            else:
+                bad_match_count += 1
+        
+        percent_good = match_count/len(ref_ids)
+        percent_bad = bad_match_count/len(ref_ids)
+
+        print(f"\nRef clusters matched: {match_count}")
+        print(f"Ref clusters unmatched: {bad_match_count}")
+        print(f"{np.round(percent_good*100, 2)}% matched, {np.round(percent_bad*100, 2)}% unmatched.")
+        
+        match_dict = dict(sorted(match_dict.items()))
+
+        return match_dict
+
+def get_protocol_from_datafile(exp_name, datafile_name):
+    exp_summary = dju.get_mea_exp_summary(exp_name)
+    protocol_name = exp_summary.query('datafile_name == @datafile_name').reset_index(drop = True)
+    return protocol_name.loc[0,'protocol_name']
+
 def get_classification_file_path(classification_file_name: str, exp_name: str, chunk_name: str, 
-                                 nas_analysis_dir: str = NAS_ANALYSIS_DIR, ks_version: str = 'kilosort2.5'):
+                                 ss_version: str = 'kilosort2.5'):
     
-    analysis_path = get_analysis_path(exp_name, chunk_name, nas_analysis_dir)
-    classification_file_path = os.path.join(analysis_path, ks_version,
-                                 classification_file_name)
+    classification_file_path = os.path.join(NAS_ANALYSIS_DIR, exp_name, chunk_name, ss_version, classification_file_name)
     
     return classification_file_path
 
-def get_analysis_path(exp_name: str, chunk_name: str, nas_analysis_dir: str = NAS_ANALYSIS_DIR) -> str:
-    """
-    Constructs the full path to the analysis directory for a specific sorting chunk.
-    """
-
-    analysis_path = os.path.join(nas_analysis_dir, exp_name, chunk_name)
-    
-    return analysis_path
-
-
-def get_data_path(exp_name: str, target_protocol: str, protocol_index: int = 0, nas_data_dir: str = NAS_DATA_DIR) -> str:
-    """
-    Constructs the full path to the data directory for a specific experiment protocol.
-    """
-
-    target_protocol_options: np.ndarray = dju.search_protocol(target_protocol)
-    try:
-        target_protocol = target_protocol_options[0]
-    except:
-        pass
-
-    experiment_summary = dju.get_mea_exp_summary(exp_name)
-
-    protocol_df = experiment_summary[(experiment_summary['protocol_name'] == target_protocol)].iloc[protocol_index]
-
-    data_val = protocol_df['data_dir']
-    
-    data_path = os.path.join(nas_data_dir, data_val)
-
-    return data_path
-
-def get_vcd(exp_name: str, data_name: str, ks_version: str = 'kilosort2.5',
-            data_index: int = 0, ei: bool = True, sta: bool = False, params: bool = True,
-            runtimemovie_params: bool = True, neurons: bool = False, noise: bool = False,
-            nas_analysis_dir: str = NAS_ANALYSIS_DIR, nas_data_dir: str = NAS_DATA_DIR) -> vl.VisionCellDataTable:
-    """
-    Retrieves a VisionCellDataTable (VCD) object using the visionloader module.
-    """
-    
-    if 'chunk' in data_name:
-        # we're pulling analysis data by chunk name
-        data_path = get_analysis_path(exp_name, data_name, nas_analysis_dir)
-        data_path = os.path.join(data_path, ks_version)
-
-        vcd = []
-        vcd = vl.load_vision_data(data_path, ks_version, include_ei = ei,
-                                  include_noise = noise, include_sta = sta,
-                                  include_params = params, include_runtimemovie_params = runtimemovie_params,
-                                  include_neurons = neurons)
-        
-    else:
-        # we're pulling data for a protocol
-        data_path = get_data_path(exp_name, data_name, data_index, nas_data_dir)
-        data_val = os.path.basename(data_path)
-        data_path = os.path.join(data_path, ks_version)
-        
-
-        vcd = []
-        vcd = vl.load_vision_data(data_path, data_val, include_ei = ei,
-                                  include_noise = noise, include_sta = sta,
-                                  include_params = False, include_runtimemovie_params = False,
-                                  include_neurons = neurons)
-        
-    return vcd
-
-
-def find_matches(exp_name: str, ref_data: str, match_data: str, corr_cutoff: float = 0.7,
-                 ks_version: str = 'kilosort2.5', ref_index: int = 0,  match_index: int = 0,
-                 use_ei: bool = True, use_sta: bool = False, use_params: bool = False,
-                 use_neurons: bool = False, use_noise = False, use_isi = False, nas_analysis_dir: str = NAS_ANALYSIS_DIR,
-                 nas_data_dir: str = NAS_DATA_DIR, classification_file: str = "dragos_autoClassification.txt"):
-    """
-    Finds matching cells between two sorting chunks (reference and match) based on electrical images (EIs)
-    and optionally other features.
-
-    This function uses Vision Cell Data Table (VCD) objects to load and compare cell data
-    from two different sources: a reference dataset and a dataset to find matches in.
-    It calculates the cross-correlation between EIs to identify potential matches and
-    applies additional criteria (if specified) to filter out bad matches.
-    """
-
-    print("\nLoading reference data...")
-    ref_vcd = get_vcd(exp_name, ref_data, ks_version, ref_index,
-                    use_ei, use_sta, use_params,
-                    neurons = use_neurons, noise = use_noise, 
-                    nas_analysis_dir = nas_analysis_dir, nas_data_dir = nas_data_dir)
-    
-    print("\nLoading matching data...")
-    test_vcd = get_vcd(exp_name, match_data, ks_version,
-                        match_index, use_ei, use_sta, use_params,
-                        neurons = use_neurons, noise = use_noise,
-                        nas_analysis_dir = nas_analysis_dir, nas_data_dir = nas_data_dir)
-
-
-    ref_ids = ref_vcd.get_cell_ids()
-
-    test_ids = test_vcd.get_cell_ids()
-
-    print(f"\nNumber of cells found in {ref_data}: {len(ref_ids)}")
-    print(f"Number of cells found in {match_data}: {len(test_ids)}")
-
-    cell_idx = dict()
-    match_count = 0
-    bad_match_count = 0
-    sta_corr = 1
-    isi_corr = 1
-
-    corr = ei_corr(ref_vcd, test_vcd)
-
-    for idx, ref_cell in enumerate(ref_ids):
-        sorted_corr = np.sort(corr[idx,:])
-        sorted_corr = np.flip(sorted_corr)
-
-        max_corr = sorted_corr[0]
-        next_max_corr = sorted_corr[1]
-        max_ind = np.argmax(corr[idx,:])
-        max_rev_ind = np.argmax(corr[:,max_ind])
-
-        if max_corr > corr_cutoff:
-            if (use_sta):
-                ref_sta = ref_vcd.get_sta_for_cell(ref_cell)
-                ref_sta.red[abs(ref_sta.red) < (ref_sta.red.std()*1.5)] = 0
-                startInt = ref_sta.red[1, 1, :].size - 15
-                endInt = startInt + 10                                      
-                match_sta = test_vcd.get_sta_for_cell(test_ids[max_ind])            
-                match_sta.red[abs(match_sta.red) < (match_sta.red.std()*1.5)] = 0
-                sta_corr = (np.corrcoef(ref_sta.red[:,:,startInt:endInt].flatten(), match_sta.red[:,:,startInt:endInt].flatten())[0,1] +
-                    np.corrcoef(ref_sta.blue[:,:,startInt:endInt].flatten(), match_sta.blue[:,:,startInt:endInt].flatten())[0,1] )/2.0
-                
-            if (use_isi):
-                ref_isi = ref_vcd.get_acf_numpairs_for_cell(ref_cell)
-                match_isi = test_vcd.get_acf_numpairs_for_cell(test_ids[max_ind])
-                np.nan_to_num(ref_isi, copy=False, nan=0, neginf=0, posinf=0)
-                np.nan_to_num(match_isi, copy = False, nan=0, neginf=0, posinf=0)
-                isi_corr = np.corrcoef(ref_isi, match_isi)[0,1]
-
-            if next_max_corr > (max_corr*0.90) or sta_corr < 0 or isi_corr < 0.2:
-                bad_match_count += 1
-            elif ref_ids[max_rev_ind] != ref_cell:
-                bad_match_count += 1
-                # print(f"{ref_cell} is not the best match for {test_ids[max_ind]}.")
-            else:
-                cell_idx[ref_cell] = test_ids[max_ind]
-                match_count += 1    
-
-        else:
-            bad_match_count += 1
-    
-    percent_good = match_count/len(ref_ids)
-    percent_bad = bad_match_count/len(ref_ids)
-
-    print(f"\nRef clusters matched: {match_count}")
-    print(f"Ref clusters unmatched: {bad_match_count}")
-    print(f"{np.round(percent_good*100, 2)}% matched, {np.round(percent_bad*100, 2)}% unmatched.")
-    
-    cell_idx_sorted = dict(sorted(cell_idx.items()))
-
-    return cell_idx_sorted, ref_ids, test_ids
-
-def auto_classification(exp_name: str, ref_data: str, match_data: str, classification_file: str,
-                            corr_cutoff: float = 0.8, ks_version: str = 'kilosort2.5', ref_index: int = 0,
-                            match_index: int = 0, use_ei: bool = True, use_sta: bool = False, use_params: bool = False,
-                            use_neurons: bool = False, use_noise: bool = False, use_isi: bool = False,
-                            output_filename: str = 'dragos_autoClassification.txt', nas_analysis_dir: str = NAS_ANALYSIS_DIR,
-                            nas_data_dir: str = NAS_DATA_DIR):
-    """
-    Finds matching cells between two sorting chunks (reference and match) based on electrical images (EIs)
-    and creates a classification file using a given noise classification chunk.
-    """
-        
-    match_dict, ref_ids, test_ids = find_matches(exp_name, ref_data, match_data, corr_cutoff, ks_version,
-                              ref_index, match_index, use_ei, use_sta, use_params,
-                              use_neurons, use_noise, use_isi, nas_analysis_dir, nas_data_dir)
-    
-    classification_file_path = get_classification_file_path(classification_file, exp_name, ref_data, nas_analysis_dir, ks_version)
-    print(f"\nUsing {classification_file} classification file for {ref_data}")
-
-    matched_count = 0
-    unmatched_count = 0
-    arr_types = create_dictionary_from_file(classification_file_path, delimiter = ' ')
-
-    with open(output_filename, mode='w') as output_file:
-        for key in match_dict.keys():
-            matched_count += 1
-            print(match_dict[key], arr_types[key], file = output_file)
-
-    partial_output = create_dictionary_from_file(output_filename, delimiter = ' ')
-
-    with open(output_filename, mode = 'a') as output_file:
-        for test_id in test_ids:
-            if test_id in partial_output:
-                pass
-            else:
-                print(test_id, 'All/unmatched', file = output_file)
-                unmatched_count += 1
-
-    percent_good = matched_count/len(test_ids)
-    percent_bad = unmatched_count/len(test_ids)
-
-    print(f"\nTarget clusters matched: {matched_count}\nTarget clusters unmatched: {unmatched_count}")
-    print(f"{np.round(percent_good*100, 2)}% matched, {np.round(percent_bad*100, 2)}% unmatched.\n")
-
-    match_dict_sorted = dict(sorted(match_dict.items()))
-
-    return match_dict_sorted
 
 def ei_corr(ref_vcd: vl.VisionCellDataTable, target_vcd: vl.VisionCellDataTable, method: str = 'full') -> np.ndarray:
 
@@ -280,38 +169,6 @@ def create_dictionary_from_file(file_path, delimiter=' '):
             result_dict[int(key)] = value
     
     return result_dict
-    
-
-
-def find_nearest_noise(exp_name: str, target_protocol: str, protocol_index: int = 0) -> str:
-    
-    experiment_summary = dju.get_mea_exp_summary(exp_name)
-    noise_protocol = dju.search_protocol('.SpatialNoise')[0]
-    target_protocol_name = dju.search_protocol(target_protocol)[0]
-    
-    noise_condition_1 = experiment_summary['protocol_name'] == noise_protocol
-    noise_condition_2 = experiment_summary['chunk_name'].str.contains('chunk')
-    noise_runs = experiment_summary[noise_condition_1 & noise_condition_2]
-
-    target_condition_1 = experiment_summary['protocol_name'] == target_protocol_name
-    target_run = experiment_summary[target_condition_1].iloc[protocol_index]
-
-    target_run_start = target_run['minutes_since_start']-target_run['duration_minutes']
-    target_run_stop = target_run['minutes_since_start']
-
-    noise_run_start = noise_runs['minutes_since_start']-noise_runs['duration_minutes']
-    noise_run_stop = noise_runs['minutes_since_start']
-
-    protocolstop_to_noisestart = abs(noise_run_start - target_run_stop)
-    protocolstart_to_noisestop = abs(noise_run_stop - target_run_start)
-
-    minimum_distance = np.minimum(protocolstart_to_noisestop, protocolstop_to_noisestart)
-
-    closest_noise_chunk = noise_runs[(protocolstart_to_noisestop == min(minimum_distance))]
-    if closest_noise_chunk.empty:
-        closest_noise_chunk = noise_runs[(protocolstop_to_noisestart == min(minimum_distance))]
-
-    return closest_noise_chunk.reset_index().at[0,'chunk_name']
 
 def get_presentation_times(frame_times, preFrames, flashFrames, gapFrames, images_per_epoch):
     flash_times = []
