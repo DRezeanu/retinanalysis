06dcda982053c8d6ea5ef7e3c14d29efc5f52e4b
DRezeanu <DRezeanu@uw.edu>
2025-07-21 17:27:33 -0700

    Update vision_utils.py

--- src/retinanalysis/utils/vision_utils.py
+++ src/retinanalysis/utils/vision_utils.py
@@ -522,7 +522,16 @@ def create_dictionary_from_file(file_path, delimiter=' '):
     
     return result_dict
 
-def get_presentation_times(frame_times, preFrames, flashFrames, gapFrames, images_per_epoch):
+def get_presentation_times(frame_times: np.ndarray, preFrames:int, flashFrames: int,
+                           gapFrames: int, images_per_epoch: int):
+
+    # Ensure frame times are in integers
+    preFrames = int(preFrames)
+    flashFrames = int(flashFrames)
+    gapFrames = int(gapFrames)
+    images_per_epoch = int(images_per_epoch)
+
+
     flash_times = []
     gap_times = []
 
