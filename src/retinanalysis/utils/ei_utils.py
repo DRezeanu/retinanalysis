88422aab69b2fe1aed6583f44009e778f4344494
Vyom Raval <vyomr@uw.edu>
2025-09-22 16:32:11 -0700

    refactor

--- src/retinanalysis/utils/ei_utils.py
+++ src/retinanalysis/utils/ei_utils.py
@@ -91,12 +91,89 @@ def get_ei_and_map(n_ID: int, vcd: VisionCellDataTable):
     ei_map = np.max(np.abs(ei), axis=2)
     return ei, ei_map
 
+def plot_ei_spatial_map(ei_map, sorted_electrodes, top_idx, axs=None, n_ID=None, vcd=None):
+    """
+    Plot the spatial EI map and highlight the peak and selected electrodes.
+    """
+    n_markers = len(top_idx)
+    if axs is None:
+        f, ax = plt.subplots(figsize=(6, 4))
+    else:
+        ax = axs
+
+    im = ax.imshow(ei_map, cmap='hot', aspect='auto')
+    plt.colorbar(im, ax=ax, label='log10(abs(EI amplitude))')
+
+    # Get index of peak
+    peak_channel = np.argmax(ei_map)
+    peak_idx = np.unravel_index(peak_channel, ei_map.shape)
+    peak_channel_idx = sorted_electrodes[peak_channel]
+    ax.plot(peak_idx[1], peak_idx[0], 'o', color='blue')
+    ax.axhline(peak_idx[0], color='blue')
+    ax.axvline(peak_idx[1], color='blue')
+
+    for i in range(n_markers):
+        top = top_idx[i]
+        channel_idx = sorted_electrodes[top]
+        y, x = np.unravel_index(top, ei_map.shape)
+        ax.plot(x, y, 'o', color='C2', ms=5)
+        ax.text(x, y, str(i), color='k')
+
+    str_title = ''
+    if n_ID is not None and vcd is not None:
+        sample_rate = 20000.0 # Hz
+        sts = vcd.get_spike_times_for_cell(n_ID)
+        num_sps = len(sts)
+        max_st = sts.max()/sample_rate
+        avg_rate = num_sps/max_st
+        str_title += f'ID {n_ID}\nPeak: {peak_idx[0].item(), peak_idx[1].item()}, e{peak_channel_idx}\n{num_sps} sps ({avg_rate:.1f} Hz)\n'
+    ax.set_title(str_title)
+    return ax
+
+def plot_ei_timeseries(ei, sorted_electrodes, top_idx, 
+axs=None, c='C2', label=None, b_vline=True, b_title=False):
+    """
+    Plot the EI timeseries for the selected electrodes.
+    """
+    n_markers = len(top_idx)
+    if axs is None:
+        f, axs = plt.subplots(nrows=n_markers, figsize=(6, 4))
+
+    if n_markers == 1:
+        axs = [axs]
+
+    for i in range(n_markers):
+        top = top_idx[i]
+        channel_idx = sorted_electrodes[top]
+        y, x = np.unravel_index(top, ei.shape[:2])
+        ei_ts = ei[y, x, :]
+        ax = axs[i]
+        ax.plot(ei_ts, c, label=label)
+        
+        ax.set_xticks([])
+        if b_title:
+            ax.set_title(f'{i} (e{channel_idx})')
+        else:
+            ax.set_ylabel(f'{i} (e{channel_idx})')
+        if i == n_markers - 1:
+            ax.set_xticks(np.arange(0, len(ei_ts), 50))
+            ax.set_xlabel('Timeframe')
+
+        if b_vline:
+            ax.axvline(np.argmin(ei_ts), color='k')
+
+    return axs
+
 def plot_ei_map(n_ID: int, vcd: VisionCellDataTable, top_idx=None, 
-                axs=None, label=None):
+                axs=None, n_interval=2, n_markers=5, 
+                **ts_kwargs):
+    
     sorted_electrodes = sort_electrode_map(vcd.get_electrode_map())
     if top_idx is None:
         # Get top electrodes if not provided
-        top_idx = get_top_electrodes(n_ID, vcd)
+        top_idx = get_top_electrodes(
+            n_ID, vcd, n_interval=n_interval, 
+            n_markers=n_markers, b_sort=True)
 
     ei, ei_map = get_ei_and_map(n_ID, vcd)
     # Log is better for visualization
@@ -104,51 +181,26 @@ def plot_ei_map(n_ID: int, vcd: VisionCellDataTable, top_idx=None,
     
     # top_idx is the top n_markers pixels to plot, returned by get_top_electrodes
     n_markers = len(top_idx)
+    # Prepare axes if not provided
     if axs is None:
-        f, axs = plt.subplots(nrows=n_markers+1, figsize=(6, 8),
-                              gridspec_kw={'height_ratios': [1]+[1/n_markers]*n_markers})
+        fig, axs = plt.subplots(
+            nrows=n_markers + 1,
+            figsize=(6, 8),
+            gridspec_kw={'height_ratios': [1] + [1 / n_markers] * n_markers}
+        )
 
-    sample_rate = 20000.0 # Hz
-    sts = vcd.get_spike_times_for_cell(n_ID)
-    num_sps = len(sts)
-    max_st = sts.max()/sample_rate
-    avg_rate = num_sps/max_st
+    # Plot spatial map on first axis
+    plot_ei_spatial_map(
+        ei_map, sorted_electrodes, top_idx,
+        axs=axs[0], n_ID=n_ID, vcd=vcd
+    )
 
-    ax0 = axs[0]
-    im=ax0.imshow(ei_map, cmap='hot', aspect='auto')
-    plt.colorbar(im, ax=ax0, label='log10(abs(EI amplitude))')
+    # Plot timeseries on remaining axes
+    if n_markers > 0:
+        plot_ei_timeseries(
+            ei, sorted_electrodes, top_idx, axs=axs[1:], 
+            **ts_kwargs
+        )
 
-    # Get index of peak
-    peak_channel = np.argmax(ei_map)
-    peak_idx = np.unravel_index(peak_channel, ei_map.shape)
-    peak_channel_idx = sorted_electrodes[peak_channel]
-    ax0.plot(peak_idx[1], peak_idx[0], 'o', color='blue')
-    ax0.axhline(peak_idx[0], color='blue')
-    ax0.axvline(peak_idx[1], color='blue')
-
-    for i in range(n_markers):
-        top = top_idx[i]
-        channel_idx = sorted_electrodes[top]
-        y, x = np.unravel_index(top, ei_map.shape)
-        ax0.plot(x, y, 'o', color='C2', ms=5)
-        ax0.text(x, y, str(i), color='k')
-
-        ax = axs[i+1]
-        # ei_ts = ei_grid[:, y, x]
-        ei_ts = ei[y, x, :]
-        ax.plot(ei_ts, 'C2')
-        ax.axvline(np.argmin(ei_ts), color='k')
-        ax.set_xticks([])
-        ax.set_ylabel(f'{i} (e{channel_idx})')
-    
-    # Set xticks for last ax
-    ax.set_xticks(np.arange(0, len(ei_ts), 50))
-    ax.set_xlabel('Timeframe')
-
-    str_title = ''
-    if label is not None:
-        str_title += f'{label} '
-    str_title += f'ID {n_ID}\nPeak: {peak_idx}, e{peak_channel_idx}\n{num_sps} sps ({avg_rate:.1f} Hz)\n'
-    ax0.set_title(str_title)
     plt.tight_layout()
-    return ax0
+    return axs
