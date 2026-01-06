3a750034b450490f3525b7750ff50339778dfeb0
DRezeanu <DRezeanu@uw.edu>
2026-01-06 14:27:46 -0800

    minor bug fix in get_spike_xarr

--- src/retinanalysis/utils/vision_utils.py
+++ src/retinanalysis/utils/vision_utils.py
@@ -385,7 +385,7 @@ def get_spike_xarr(response_block: MEAResponseBlock, protocol_ids: Optional[List
 
     coords = {'epoch' : np.arange(response_block.n_epochs),
             'cell_id' : filtered_df['cell_id'].values,
-            'cell_type' : ('cell_id', filtered_df['cell_type'].values),
+            'cell_type' : ('cell_id', np.asarray(filtered_df['cell_type'].values, dtype = 'U')),
             'noise_id' : ('cell_id', filtered_df['noise_id'].values)}
 
     spike_time_xarr = xr.DataArray(spike_time_arr, dims = dims, coords = coords)
