b5f45711ec97f59247179837f52ec67e8f5cde7b
vmr160030 <vyomr22@gmail.com>
2026-05-04 13:49:20 -0700

    going back to my version entirely

--- src/retinanalysis/classes/raw.py
+++ src/retinanalysis/classes/raw.py
@@ -6,7 +6,6 @@ import retinanalysis
 from retinanalysis.classes.response import MEAResponseBlock
 from retinanalysis.config.settings import RAW_DIR
 from retinanalysis import ei_utils as eiu
-from typing import Optional
 try:
     import importlib.resources as ir
 except:
@@ -20,18 +19,59 @@ D_ELECTRODE_MAPS = {
     '60um': np.loadtxt(ir.files(retinanalysis) / "assets/electrode_maps/60um_electrode_map.txt", dtype=int)
 }
 class RawTraces:
-    # def __init__(self, rb: MEAResponseBlock):
-    def __init__(self, binpath: str):
-        self.binpath = binpath
-        # self.d_timing = rb.d_timing
-        # self.sorted_electrodes = eiu.sort_electrode_map(rb.vcd.get_electrode_map())
+    def __init__(self, rb: MEAResponseBlock=None, binpath=None):
+        if rb is None and binpath is None:
+            raise ValueError("Either rb (MEAResponseBlock) or binpath must be provided.")
+
+        if rb is None:
+            self.binpath = binpath
+            self.d_timing = {}    
+            print(f"Assuming 60um electrode map for binpath {binpath} since no MEAResponseBlock provided.")
+            self.sorted_electrodes = eiu.sort_electrode_map(D_ELECTRODE_MAPS['60um'])
+        else:
+            self.binpath = os.path.join(RAW_DIR, rb.exp_name, rb.datafile_name)
+            self.d_timing = rb.d_timing
+            self.sorted_electrodes = eiu.sort_electrode_map(rb.vcd.get_electrode_map())
+        
         self.data = None
+        # requested data variable, for holding 
+        # smaller range of data from already loaded data
+        self.r_data = None 
+        self.d_meta = {'start_sample': None, 'end_sample': None, 'channels': None}
         self.ttl_times = None
         self.ttl_samples = None
         self.sample_rate = SAMPLE_RATE  # Hz
         self.epoch_idx = None
 
-    def load_bin_data(self, start_sample=0, end_sample=None, verbose=False):
+    def load_ttl_data(self, start_sample, end_sample, verbose=False):
+        with bin2py.PyBinFileReader(self.binpath, chunk_samples=RW_BLOCKSIZE, is_row_major=True) as pbfr:
+            array_id = pbfr.header.array_id
+            total_samples = pbfr.length
+
+            # Set end_sample to the total length if not specified
+            if end_sample is None:
+                end_sample = total_samples
+
+            # Validate sample range
+            if start_sample < 0 or end_sample > total_samples or start_sample >= end_sample:
+                raise ValueError("Invalid start_sample or end_sample range.")
+
+            query_samples = end_sample - start_sample
+            if verbose:
+                print(f"Querying {query_samples} samples from {start_sample} to {end_sample} for array {array_id}.")
+                print(f'Queried time: {query_samples / SAMPLE_RATE} seconds')
+                print(f'From {start_sample / SAMPLE_RATE} to {end_sample / SAMPLE_RATE} seconds')
+
+            data = np.zeros(query_samples, dtype=np.float32)
+            for start_idx in range(start_sample, end_sample, RW_BLOCKSIZE):
+                n_samples_to_get = min(RW_BLOCKSIZE, end_sample - start_idx)
+                samples = pbfr.get_data_for_electrode(0, start_idx, n_samples_to_get)
+                data[start_idx - start_sample:start_idx - start_sample + n_samples_to_get] = samples
+
+        self.ttl_samples = data
+
+
+    def load_bin_data(self, start_sample=0, end_sample=None, verbose=False, channels=None):
         """
         Load raw .bin data into a NumPy array.
 
@@ -45,11 +85,19 @@ class RawTraces:
         """
         with bin2py.PyBinFileReader(self.binpath, chunk_samples=RW_BLOCKSIZE, is_row_major=True) as pbfr:
             # Determine the number of electrodes and total samples
-            n_channels = pbfr.num_electrodes
+            if channels is None:
+                n_channels = pbfr.num_electrodes
+                channels = np.arange(n_channels)
+            else:
+                # Ensure array
+                channels = np.array(channels)
+                n_channels = len(channels)
+                
+            
             total_samples = pbfr.length
             
             if verbose:
-                print(f"Number of electrodes: {n_channels}, Total samples: {total_samples}.")
+                print(f"Querying number of electrodes: {n_channels}, Total samples: {total_samples}.")
                 print(f"Total time: {total_samples / SAMPLE_RATE} seconds")
                 print(f"Sample rate: {SAMPLE_RATE} Hz")
 
@@ -67,11 +115,28 @@ class RawTraces:
                 print(f'Queried time: {query_samples / SAMPLE_RATE} seconds')
                 print(f'From {start_sample / SAMPLE_RATE} to {end_sample / SAMPLE_RATE} seconds')
                 
+            # Check if already loaded for this range and channels
+            if self.data is not None:
+                if (self.d_meta['start_sample'] <= start_sample and 
+                    self.d_meta['end_sample'] >= end_sample and
+                    np.array_equal(self.d_meta['channels'], channels)):
+                    if verbose:
+                        print("Requested data range and channels already loaded.")
+                    # Return relevant slice
+                    start_idx = start_sample - self.d_meta['start_sample']
+                    end_idx = end_sample - self.d_meta['start_sample']
+                    self.r_data = self.data[:, start_idx:end_idx]
+                    return
+                else:
+                    if verbose:
+                        print("Requested data range or channels differ from loaded data. Reloading.")
+                    self.data = None  # Clear existing data to load new range/channels
+            
             # Preallocate array for the data
-            data = np.zeros((n_channels, query_samples), dtype=np.float32)
+            data = np.zeros((n_channels, query_samples), dtype=np.int16)
 
             ttl_times_buffer = []
-            ttl_samples = np.zeros((query_samples,), dtype=np.float32)
+            ttl_samples = np.zeros((query_samples,), dtype=np.int16)
             # Read data in chunks
             for start_idx in range(start_sample, end_sample, RW_BLOCKSIZE):
                 n_samples_to_get = min(RW_BLOCKSIZE, end_sample - start_idx)
@@ -89,7 +154,7 @@ class RawTraces:
                 ttl_times_buffer.append(trigger_indices[:, 0])
 
                 # Populate the data matrix (exclude channel 0)
-                data[:, start_idx - start_sample:start_idx - start_sample + n_samples_to_get] = chunk[1:, :]
+                data[:, start_idx - start_sample:start_idx - start_sample + n_samples_to_get] = chunk[channels + 1, :]
                 # ttl_samples[start_idx - start_sample:start_idx - start_sample + n_samples_to_get] = chunk[0, :]
 
             # Concatenate TTL times
@@ -99,10 +164,17 @@ class RawTraces:
             print(f'Data shape: {data.shape}')
         # print(f'TTL times shape: {ttl_times.shape}')
         self.data = data
+        self.r_data = data
+        self.d_meta['start_sample'] = start_sample
+        self.d_meta['end_sample'] = end_sample
+        self.d_meta['channels'] = channels
         self.ttl_times = ttl_times
         self.ttl_samples = ttl_samples
         
     def load_epoch_index(self, epoch_idx, verbose=True):
+        # Raise error if d_timing empty
+        if not self.d_timing:
+            raise ValueError("Timing information (d_timing) is empty. Cannot load epoch index.")
         epoch_start = self.d_timing['epochStarts'][epoch_idx]
         epoch_end = self.d_timing['epochEnds'][epoch_idx]
         self.load_bin_data(start_sample=epoch_start, end_sample=epoch_end, verbose=verbose)
@@ -110,7 +182,7 @@ class RawTraces:
 
 def plot_sts_over_trace(rt: RawTraces, rb: MEAResponseBlock, 
                         cell_id, epoch_idx, start_time=0, end_time=None,
-                        n_highlight_width=18, channel_idx: Optional[int]=None, ax=None):
+                        n_highlight_width=18, channel_idx:int=None, ax=None):
     # Load epoch_idx if needed
     if rt.epoch_idx != epoch_idx:
         rt.load_epoch_index(epoch_idx, verbose=True)
@@ -160,6 +232,5 @@ def plot_sts_over_trace(rt: RawTraces, rb: MEAResponseBlock,
                 raw_ts[st:st+n_highlight_width], color='red', lw=2, alpha=0.5)
     
     
-
 
 
