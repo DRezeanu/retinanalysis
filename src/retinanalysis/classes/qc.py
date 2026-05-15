597e23363cf82dfec493657d64a76461fb006578
Oaks-Leaf Rachel <roaks@uw.edu>
2026-05-15 15:36:48 -0700

    commit local to rachel dev

--- src/retinanalysis/classes/qc.py
+++ src/retinanalysis/classes/qc.py
@@ -10,7 +10,8 @@ def get_nsps(resp: MEAResponseBlock | MEAResponseGroup | AnalysisChunk, cell_ids
 
     if isinstance(resp, AnalysisChunk):
         if len(resp.data_files) > 1:
-            resp = create_mea_response_group(resp.exp_name, resp.data_files, verbose = False)
+            # resp = create_mea_response_group(resp.exp_name, resp.data_files, verbose = False)
+            resp= MEAResponseBlock(resp.exp_name, resp.data_files[0], include_ei = False, b_load_fd = False, verbose = False)
         else:
             resp = MEAResponseBlock(resp.exp_name, resp.data_files[0], include_ei = False, b_load_fd = False, verbose = False)
 
