61b2b60572348ad1dfdfd51e6fe81aab75dcfb9d
Vyom Raval <vyomr@uw.edu>
2026-05-18 13:42:55 -0700

    docs

--- src/retinanalysis/classes/response.py
+++ src/retinanalysis/classes/response.py
@@ -819,6 +819,9 @@ def create_mea_response_group(
         b_LED (bool): Boolean value, if True assume stimulus for tehse datafiles was deliverd by an LED.
         This automatically sets b_load_fd to False since LED stimuli have no frame data. Default False.
 
+        b_load_vcd (bool): Boolean value, if True will load the vision data table.
+            Mainly for debugging purposes to skip load time.
+
         verbose (bool): Boolean value, if True will print all outputs to the console. Since multiple
         MEAResponseBlocks must be created, with many status messages, Default is False.
 
