46b9569d9cff225ced36637e950f15d1b26e7016
Vyom Raval <vyomr@uw.edu>
2026-09-23 11:33:48 -0700

    minor fix

--- src/retinanalysis/utils/regen.py
+++ src/retinanalysis/utils/regen.py
@@ -1125,7 +1125,7 @@ def make_bars_and_gain(
     if exp_date  < 20260923:
         noiseType = 'gaussian'
     else:
-        noiseType = df_epochs.loc[0, 'noiseType']
+        noiseType = df_epochs["epoch_parameters"][0]['noiseType']
     
     # if exp_date < 20250806:
     if b_lines_only:
